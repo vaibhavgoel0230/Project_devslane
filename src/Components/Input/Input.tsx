3646ad2f8af9a6832cb51ae180e1b4dd021298aa
vaibhavgoel0230 <vaibhav.goel_cs.aiml19@gla.ac.in>
2021-07-26 00:12:01 +0530

    Updated input component and created its story

--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -1,34 +1,25 @@
 import { FC, InputHTMLAttributes, memo } from "react";
+import { IconType } from "react-icons";
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   touched?: boolean;
   error?: string;
+  Icon?: IconType;
 }
 const Input: FC<Props> = ({
   touched,
   id,
   error,
   className,
+  Icon,
   placeholder,
   ...rest
 }) => {
   return (
     <>
-      <div className="relative pt-3 pb-6">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          className="absolute top-4 text-blue-700"
-        >
-          <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-          <circle cx="12" cy="7" r="4"></circle>
-        </svg>
+      <div className="relative pt-3 ">
+        {Icon && (
+          <Icon className="w-6 h-6 absolute top-3 text-blue-600 fill-blue"></Icon>
+        )}
         {id && placeholder && (
           <label htmlFor={id} className="sr-only">
             {placeholder}
@@ -38,10 +29,12 @@ const Input: FC<Props> = ({
         <input
           id={id}
           {...rest}
-          className=" inline-block align-middle text-AuthHeadColor text-md pb-2 pl-9 border-b border-solid w-full"
+          className=" inline-block align-middle outline-none text-AuthHeadColor text-md pb-4 pl-9 border-b border-solid w-full"
           placeholder={placeholder}
         />
-        {touched && <div className="text-red-500">{error}</div>}
+        <div className="w-full h-7 text-right">
+          {touched && <div className="text-red-500">{error}</div>}
+        </div>
       </div>
     </>
   );
--- src/Components/Input/input.stories.tsx
+++ src/Components/Input/input.stories.tsx
@@ -1,8 +1,26 @@
+import "../../index.css";
+import { FiLock, FiUser, FiAtSign } from "react-icons/fi";
 import Input from "./Input";
+
+const icons = { FiLock, FiUser, FiAtSign };
 
 export default {
   title: "Input",
   component: Input,
+  argTypes: {
+    Icon: {
+      options: Object.keys(icons),
+      mapping: icons,
+      control: {
+        type: "select",
+      },
+    },
+  },
 };
 
-export const defaultInput = () => <Input />;
+const Template = (args: any) => <Input {...args}></Input>;
+
+export const main: any = Template.bind({});
+main.args = {
+  placeholder: "Password",
+};
