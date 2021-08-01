5b1637779ef45a4250c46899dbc8fe415a672d7b
vaibhavgoel0230 <vaibhav.goel_cs.aiml19@gla.ac.in>
2021-08-01 14:29:56 +0530

    converted api call using fetch into api call using axios

--- src/api.tsx
+++ src/api.tsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 interface LoginData {
   email: string;
   password: string;
@@ -9,14 +11,16 @@ export const login = (data: LoginData) => {
   const url = BASE_URL + "/login";
   console.log(data);
 
-  return fetch(url, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" },
-  }).then((response) => {
-    response
-      .json()
-      .then((data) => console.log("response body in JSON Format", data));
-    return response;
-  });
+  axios.post(url, data).then((response) => console.log(response));
+
+  // return fetch(url, {
+  //   method: "POST",
+  //   body: JSON.stringify(data),
+  //   headers: { "Content-Type": "application/json" },
+  // }).then((response) => {
+  //   response
+  //     .json()
+  //     .then((data) => console.log("response body in JSON Format", data));
+  //   return response;
+  // });
 };
