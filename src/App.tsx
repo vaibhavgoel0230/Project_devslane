25ad6be0f8e8e1e9a4d3ae5150c285239f038010
vaibhavgoel0230 <vaibhav.goel_cs.aiml19@gla.ac.in>
2021-08-03 14:18:07 +0530

    created React Context to store user state

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { me } from "./api/auth";
 import { LS_AUTH_TOKEN } from "./api/base";
+import AppContext from "./App.context";
 import { User } from "./modals/User";
 import NotFoundPage from "./Pages/NotFound.page";
 
@@ -21,55 +22,57 @@ const App: React.FC<Props> = (props) => {
       return;
     }
     me().then((u) => setUser(u));
-  }, []);
+  }, []); // eslint-disable-line
 
   if (!user && token) {
     return <div>loading...</div>;
   }
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact>
-          {user ? <Redirect to="/dashboard" /> : <Redirect to="/login" />}
-        </Route>
-        <Route path={["/login", "/signup"]} exact>
-          {user ? (
-            <Redirect to="/dashboard" />
-          ) : (
+    <AppContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            {user ? <Redirect to="/dashboard" /> : <Redirect to="/login" />}
+          </Route>
+          <Route path={["/login", "/signup"]} exact>
+            {user ? (
+              <Redirect to="/dashboard" />
+            ) : (
+              <Suspense
+                fallback={
+                  <div className="text-indigo-600 text-3xl p-4 m-auto">
+                    Content is Loading
+                  </div>
+                }
+              >
+                <AuthPageLazy onLogin={setUser} />
+              </Suspense>
+            )}
+          </Route>
+          <Route
+            path={[
+              "/dashboard",
+              "/recordings",
+              "/batch/:batchNumber/lecture/:lectureNumber",
+            ]}
+            exact
+          >
             <Suspense
               fallback={
-                <div className="text-indigo-600 text-3xl p-4 m-auto">
+                <div className="text-indigo-600 text-3xl w-full p-4 m-auto">
                   Content is Loading
                 </div>
               }
             >
-              <AuthPageLazy onLogin={setUser} />
+              {user ? <AppContainerPageLazy /> : <Redirect to="/login" />}
             </Suspense>
-          )}
-        </Route>
-        <Route
-          path={[
-            "/dashboard",
-            "/recordings",
-            "/batch/:batchNumber/lecture/:lectureNumber",
-          ]}
-          exact
-        >
-          <Suspense
-            fallback={
-              <div className="text-indigo-600 text-3xl w-full p-4 m-auto">
-                Content is Loading
-              </div>
-            }
-          >
-            {user ? <AppContainerPageLazy /> : <Redirect to="/login" />}
-          </Suspense>
-        </Route>
-        <Route>
-          <NotFoundPage></NotFoundPage>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+          </Route>
+          <Route>
+            <NotFoundPage></NotFoundPage>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
