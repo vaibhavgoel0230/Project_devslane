7f87d1f5a7fee30b46239ac51a544a573d7a99db
vaibhavgoel0230 <vaibhav.goel_cs.aiml19@gla.ac.in>
2021-08-03 14:46:54 +0530

    added menu bar

--- src/Pages/AppContainer.page.tsx
+++ src/Pages/AppContainer.page.tsx
@@ -5,7 +5,7 @@ import DashboardPage from "./Dashboard.pages";
 import RecordingsPage from "./Recordings.page";
 import LecturePage from "./Lecture.page";
 import logo from "./media/logo.svg";
-import { FiSearch } from "react-icons/fi";
+import { FiChevronDown, FiMenu, FiSearch } from "react-icons/fi";
 import Input from "../Components/Input/Input";
 import Avatar from "../Components/Avatar/Avatar";
 
@@ -33,6 +33,20 @@ const AppContainer: FC<Props> = (props) => {
         </div>
         <div className="w-full flex justify-end py-1 pr-3">
           <Avatar size="small" showlivePointer={false} rounded={false} />
+        </div>
+      </div>
+      <div className="flex items-center justify-between pl-8 pr-4 py-2 bg-gray-50">
+        <div className="flex items-center">
+          <FiMenu className="w-5 h-5 mr-8" />
+          <a href="#" className="text-sm">
+            Dashboard
+          </a>
+        </div>
+        <div className="flex py-1.5 px-2 items-center justify-center bg-white border-2 border-gray-300 rounded-md">
+          <a href="#" className="px-4 text-sm">
+            Settings
+          </a>
+          <FiChevronDown />
         </div>
       </div>
       <div className="flex flex-row">
